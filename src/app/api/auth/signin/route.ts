060bb46f8a5668a1dd17fa603d4a458dc496e98b
adenirannoah1234 <adenirannoah9@gmail.com>
2024-09-08 18:30:13 +0100

    fixed the main build errors

--- src/app/api/auth/signin/route.ts
+++ src/app/api/auth/signin/route.ts
@@ -19,7 +19,7 @@ export async function POST(request: Request) {
     const body = await request.json();
     const { email, password } = SignInSchema.parse(body);
 
-    // Find user
+    // checkig if user exists
     const user = await prisma.user.findUnique({ where: { email } });
     if (!user) {
       return NextResponse.json({ message: 'Invalid email or password' }, { status: 401 });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,13 @@ import { HStack, Text } from '@chakra-ui/react';
 export default function Home() {
   return (
     <HStack>
-      <Text>Welcome</Text>
-      <Text>Welcome</Text>
-      <Text>Welcome</Text>
-      <Text>Welcome</Text>
-      <Text>Welcome</Text>
-      <Text>Welcome</Text>
-      <Text>Welcome</Text>
+      <Text>Welcome to BazaarX!</Text>
+      <Text>Welcome to BazaarX!</Text>
+      <Text>Welcome To BazaarX!</Text>
+      <Text>Welcome To BazaarX!</Text>
+      <Text>Welcome To BazaarX!</Text>
+      <Text>Welcome To BazaarX!</Text>
+      <Text>Welcome To BazaarX</Text>
     </HStack>
   );
 }
